bc7f09eaf2cad576a6fb5662cb52c9e949ee164b
Björn Steinhagen <steinhagen.bjoern@gmail.com>
2023-03-22 13:10:44 +0100

    Create dependencies.py

---
+++ dependencies.py
@@ -0,0 +1,24 @@
+import sys
+
+# Dependency check
+try:
+    import specklepy
+    import pandas
+
+except:
+    print('One of the required modules is not installed in your Python env.')
+    instSUDS = input('\nDo you want to install all dependencies and check all their versions (y/n)? ')
+    instSUDS = instSUDS.lower()
+    if instSUDS == 'y':
+        import subprocess
+        try:
+            subprocess.call('python -m pip install --upgrade pip')
+            subprocess.call('python -m pip install pandas specklepy --user')
+        except:
+            print('WARNING: Installation of modules failed!')
+            print('Please use command "python -m pip install pandas specklepy --user" in your Command Prompt.')
+            input('Press Enter to exit...')
+            sys.exit()
+    else:
+        input('Press Enter to exit...')
+        sys.exit()
